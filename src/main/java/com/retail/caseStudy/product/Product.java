95f42bbc63f1a0e06a42e77c159706f1dd07bbf6
Ryan Jepson <RyanJepson.webdev@gmail.com>
2022-09-20 08:59:43 -0600

    Setting up Products

---
+++ src/main/java/com/retail/caseStudy/product/Product.java
@@ -0,0 +1,36 @@
+package com.retail.caseStudy.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String image;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+
+}
