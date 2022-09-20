cdba2f7103e35026f7d213e74424d11b30c1f8f2
Ryan Jepson <RyanJepson.webdev@gmail.com>
2022-09-20 09:27:02 -0600

    Continued progress on products

--- src/main/java/com/retail/caseStudy/product/ProductController.java
+++ src/main/java/com/retail/caseStudy/product/ProductController.java
@@ -1,9 +1,46 @@
 package com.retail.caseStudy.product;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
+
+    @Autowired
+    ProductRepository proRep;
+
+    @GetMapping("/product")
+    public List<Product> getAllProducts() {
+        return proRep.findAll();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        Optional<Product> product = proRep.findById(id);
+        if (product.isPresent()) {
+            return product.get();
+        } else throw new ProductNotFoundException("id-" + id);
+    }
+
+    //Needs Authentication
+    @PostMapping("product")
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        Product savedPro = proRep.save(product);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPro.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+
 }
